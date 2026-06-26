0beaab461da1d1f1ad166629b33b2304a5edd28a
cfczqzy <147072115+cfczqzy@users.noreply.github.com>
2026-06-26 15:55:01 +0800

    Add files via upload

---
+++ 汇丰/ack/variables.tf
@@ -0,0 +1,45 @@
+variable "alicloud_access_key" {
+  type = string
+}
+variable "alicloud_secret_key" {
+     type = string
+}
+
+variable "region" {
+   type = string
+}
+
+variable "cluster_name" {
+    type  = string
+   default = "myk8s"
+}
+
+variable "cluster_addons" {
+  type = list(object({ 
+  name   = string
+  config = string
+  }))
+
+  default = [
+   {
+    "name"  = "flannel",
+    "config" = "",
+   },
+   {
+    "name"   = "flexvolume",
+    "config" = "",
+   },
+   {
+    "name"   = "alicloud-disk-controller",
+   "config" = "",
+   },
+   {
+    "name"  = "logtail-ds",
+    "config" = "{\"IngressDashboardEnabled\":\"true\"}",
+    },
+   {
+    "name"   = "nginx-ingress-controller",
+    "config" = "{\"IngressSlbNetworkType\":\"internet\"}",
+    }
+   ]
+}
